20548850bd46f07c6acde200b6f29ff1992665ad
Vitor-ext <Santanav92@gmail.com>
2022-10-28 18:06:34 -0300

    "Avanços"

---
+++ model/agendamento.js
@@ -0,0 +1,29 @@
+// Importação do Sequelize 
+const Sequelize = require('sequelize');
+
+// Importação da conexão com o banco
+const connection = require('../database/databese');
+
+const Agendamento = connection.define(
+    'tbl_agendamento',    // Nome da Tabela no Banco de Dados
+    {
+        cliente:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        horario:{
+            type: Sequelize.TIME,
+            allowNull: false
+        },
+        medico:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+
+    }
+);
+
+/*Executar a criação da tabela no Banco de Dados   -   Somente uma vez, após isso comentar essa linha*/
+//Agendamento.sync({force:true});
+
+module.exports = Agendamento;
---
+++ model/cliente.js
@@ -0,0 +1,34 @@
+// Importação do Sequelize 
+const Sequelize = require('sequelize');
+
+// Importação da conexão com o banco
+
+const connection = require("../database/databese");
+
+const Cliente = connection.define(
+    'tbl_cliente',    // Nome da Tabela no Banco de Dados
+    {
+        nome:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        cpf:{
+            type: Sequelize.INTEGER(11),
+            allowNull: false
+        },
+        rg:{
+            type: Sequelize.INTEGER(10),
+            allowNull: false
+        },
+        telefone:{
+            type: Sequelize.INTEGER(11),
+            allowNull: false
+        }
+    }   
+);
+
+
+/*Executar a criação da tabela no Banco de Dados   -   Somente uma vez, após isso comentar essa linha*/
+//Cliente.sync({force:True});
+
+exports.module = Cliente;
