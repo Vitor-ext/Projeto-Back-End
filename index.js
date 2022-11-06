d9dd0ab0c9c978d452b2ef2ed5f09ec13127634c
Vitor-ext <Santanav92@gmail.com>
2022-11-06 17:35:40 -0300

    "Back-End"

--- index.js
+++ index.js
@@ -10,12 +10,32 @@ app.use(express.urlencoded({extended:true}));
 
 /*Importação da conexão com o banco de dados*/
 
+const connection = require('./database/database');
 
 /* Importar Models */
+const Agendamento = require('./model/agendamento');
+const Cliente = require('./model/cliente');
+const Consulta = require('./model/consulta');
+const Dentista = require('./model/dentista');
+const Especialidade = require('./model/especialidade');
 
 
 /* Importar Rotas */
 
+const agendamentoController = require('./controller/agendamentoController');
+app.use('/', agendamentoController); 
+
+const clienteController = require('./controller/clienteController');
+app.use('/', clienteController);
+
+const consultaController = require('./controller/consultaControlle');
+app.use('/', consultaController);
+
+const dentistaController = require('./controller/dentistaController');
+app.use('/', dentistaController);
+
+const especialidadeController = require('./controller/especialidadeController');
+app.use('/', especialidadeController); 
 
 /*Servidor de requisições da aplicação */
 app.listen(3000, ()=>{
